d7281714c6013f4b30a87ea9de9259c3ab051986
marruda-3thix <marruda@3thix.com>
2025-07-07 15:49:08 -0700

    mint NFT

---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/blocto/solana-go-sdk/client"
+	"github.com/blocto/solana-go-sdk/common"
+	"github.com/blocto/solana-go-sdk/pkg/pointer"
+	"github.com/blocto/solana-go-sdk/program/associated_token_account"
+	"github.com/blocto/solana-go-sdk/program/metaplex/token_metadata"
+	"github.com/blocto/solana-go-sdk/program/system"
+	"github.com/blocto/solana-go-sdk/program/token"
+	"github.com/blocto/solana-go-sdk/rpc"
+	"github.com/blocto/solana-go-sdk/types"
+
+	"github.com/tyler-smith/go-bip39"
+)
+
+func mintNFT(c *client.Client, feePayer, mint, collection types.Account) {
+	ata, _, err := common.FindAssociatedTokenAddress(feePayer.PublicKey, mint.PublicKey)
+	if err != nil {
+		log.Fatalf("failed to find a valid ata, err: %v", err)
+	}
+
+	tokenMetadataPubkey, err := token_metadata.GetTokenMetaPubkey(mint.PublicKey)
+	if err != nil {
+		log.Fatalf("failed to find a valid token metadata, err: %v", err)
+
+	}
+	tokenMasterEditionPubkey, err := token_metadata.GetMasterEdition(mint.PublicKey)
+	if err != nil {
+		log.Fatalf("failed to find a valid master edition, err: %v", err)
+	}
+
+	mintAccountRent, err := c.GetMinimumBalanceForRentExemption(context.Background(), token.MintAccountSize)
+	if err != nil {
+		log.Fatalf("failed to get mint account rent, err: %v", err)
+	}
+
+	recentBlockhashResponse, err := c.GetLatestBlockhash(context.Background())
+	if err != nil {
+		log.Fatalf("failed to get recent blockhash, err: %v", err)
+	}
+
+	tx, err := types.NewTransaction(types.NewTransactionParam{
+		Signers: []types.Account{mint, feePayer},
+		Message: types.NewMessage(types.NewMessageParam{
+			FeePayer:        feePayer.PublicKey,
+			RecentBlockhash: recentBlockhashResponse.Blockhash,
+			Instructions: []types.Instruction{
+				system.CreateAccount(system.CreateAccountParam{
+					From:     feePayer.PublicKey,
+					New:      mint.PublicKey,
+					Owner:    common.TokenProgramID,
+					Lamports: mintAccountRent,
+					Space:    token.MintAccountSize,
+				}),
+				token.InitializeMint(token.InitializeMintParam{
+					Decimals:   0,
+					Mint:       mint.PublicKey,
+					MintAuth:   feePayer.PublicKey,
+					FreezeAuth: &feePayer.PublicKey,
+				}),
+				token_metadata.CreateMetadataAccountV3(token_metadata.CreateMetadataAccountV3Param{
+					Metadata:                tokenMetadataPubkey,
+					Mint:                    mint.PublicKey,
+					MintAuthority:           feePayer.PublicKey,
+					Payer:                   feePayer.PublicKey,
+					UpdateAuthority:         feePayer.PublicKey,
+					UpdateAuthorityIsSigner: true,
+					IsMutable:               true,
+					Data: token_metadata.DataV2{
+						Name:                 "Fake SMS #1355",
+						Symbol:               "FSMB",
+						Uri:                  "https://34c7ef24f4v2aejh75xhxy5z6ars4xv47gpsdrei6fiowptk2nqq.arweave.net/3wXyF1wvK6ARJ_9ue-O58CMuXrz5nyHEiPFQ6z5q02E",
+						SellerFeeBasisPoints: 100,
+						Creators: &[]token_metadata.Creator{
+							{
+								Address:  feePayer.PublicKey,
+								Verified: true,
+								Share:    100,
+							},
+						},
+						Collection: &token_metadata.Collection{
+							Verified: false,
+							Key:      collection.PublicKey,
+						},
+						Uses: nil,
+					},
+					CollectionDetails: nil,
+				}),
+				associated_token_account.CreateAssociatedTokenAccount(associated_token_account.CreateAssociatedTokenAccountParam{
+					Funder:                 feePayer.PublicKey,
+					Owner:                  feePayer.PublicKey,
+					Mint:                   mint.PublicKey,
+					AssociatedTokenAccount: ata,
+				}),
+				token.MintTo(token.MintToParam{
+					Mint:   mint.PublicKey,
+					To:     ata,
+					Auth:   feePayer.PublicKey,
+					Amount: 1,
+				}),
+				token_metadata.CreateMasterEditionV3(token_metadata.CreateMasterEditionParam{
+					Edition:         tokenMasterEditionPubkey,
+					Mint:            mint.PublicKey,
+					UpdateAuthority: feePayer.PublicKey,
+					MintAuthority:   feePayer.PublicKey,
+					Metadata:        tokenMetadataPubkey,
+					Payer:           feePayer.PublicKey,
+					MaxSupply:       pointer.Get[uint64](0),
+				}),
+			},
+		}),
+	})
+	if err != nil {
+		log.Fatalf("failed to new a tx, err: %v", err)
+	}
+
+	txSig, err := c.SendTransaction(context.Background(), tx)
+	if err != nil {
+		log.Fatalf("failed to send tx, err: %v", err)
+	}
+
+	fmt.Println("mint txid:", txSig)
+
+	// Wait for transaction confirmation ---
+	fmt.Println("waiting for tx confirmation...")
+	for {
+		// Get the transaction status
+		statuses, err := c.GetSignatureStatuses(context.Background(), []string{txSig})
+		if err != nil {
+			log.Printf("Failed to get signature statuses: %v", err)
+			time.Sleep(2 * time.Second) // Wait before retrying
+			continue
+		}
+
+		if len(statuses) > 0 && statuses[0] != nil {
+			if *statuses[0].ConfirmationStatus == rpc.CommitmentFinalized || *statuses[0].ConfirmationStatus == rpc.CommitmentConfirmed {
+				fmt.Println("Transaction successfully confirmed!")
+				break
+			} else {
+				fmt.Println("Transaction is being processed...")
+			}
+		} else {
+			fmt.Println("Transaction status not yet available...")
+		}
+
+		// Wait for a short period before polling again
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func main() {
+
+	mnemonic := "near industry doctor stool celery vehicle enlist symbol skate plastic ceiling zero"
+	seed := bip39.NewSeed(mnemonic, "") // (mnemonic, password)
+	feePayer, err := types.AccountFromSeed(seed[:32])
+	if err != nil {
+		log.Fatalf("failed to load feePayer account, err: %v", err)
+	}
+	fmt.Printf("feePayer: %v\n", feePayer.PublicKey.ToBase58())
+
+	c := client.NewClient(rpc.DevnetRPCEndpoint)
+
+	//show feePayer balance
+	balance, err := c.GetBalance(
+		context.TODO(),
+		feePayer.PublicKey.ToBase58(),
+	)
+	if err != nil {
+		log.Fatalf("failed to request airdrop, err: %v", err)
+	}
+	fmt.Println("balance:", balance)
+
+	mint := types.NewAccount()
+	fmt.Printf("NFT: %v\n", mint.PublicKey.ToBase58())
+
+	collection := types.NewAccount()
+	fmt.Printf("collection: %v\n", collection.PublicKey.ToBase58())
+
+	mintNFT(c, feePayer, mint, collection)
+
+	//transferNFT()
+
+}
